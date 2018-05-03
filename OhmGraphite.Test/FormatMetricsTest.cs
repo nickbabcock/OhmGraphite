e54aabb4b390b223a36415d0eb1217147144a351
Nick Babcock <nbabcock19@hotmail.com>
2018-05-03 16:21:50 -0500

    Culture agnonstic dates in tests

--- OhmGraphite.Test/FormatMetricsTest.cs
+++ OhmGraphite.Test/FormatMetricsTest.cs
@@ -10,10 +10,10 @@ namespace OhmGraphite.Test
         [Fact]
         public void FormatGraphiteIdentifier()
         {
-            var epoch = new DateTimeOffset(new DateTime(2001, 1, 13)).ToUnixTimeSeconds();
+            var epoch = new DateTimeOffset(new DateTime(2001, 1, 13), TimeSpan.Zero).ToUnixTimeSeconds();
             var sensor = new Sensor("my.cpu.identifier", "voltage", 1.06f);
             string actual = MetricTimer.FormatGraphiteData("MY-PC", epoch, sensor);
-            Assert.Equal("ohm.MY-PC.my.cpu.identifier.voltage 1.06 979365600", actual);
+            Assert.Equal("ohm.MY-PC.my.cpu.identifier.voltage 1.06 979344000", actual);
         }
 
         [Fact]
@@ -25,10 +25,10 @@ namespace OhmGraphite.Test
                 // de-DE culture will format 1.06 as 1,06 which graphite doesn't like
                 Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
 
-                var epoch = new DateTimeOffset(new DateTime(2001, 1, 13)).ToUnixTimeSeconds();
+                var epoch = new DateTimeOffset(new DateTime(2001, 1, 13), TimeSpan.Zero).ToUnixTimeSeconds();
                 var sensor = new Sensor("my.cpu.identifier", "voltage", 1.06f);
                 string actual = MetricTimer.FormatGraphiteData("MY-PC", epoch, sensor);
-                Assert.Equal("ohm.MY-PC.my.cpu.identifier.voltage 1.06 979365600", actual);
+                Assert.Equal("ohm.MY-PC.my.cpu.identifier.voltage 1.06 979344000", actual);
             }
             finally
             {
