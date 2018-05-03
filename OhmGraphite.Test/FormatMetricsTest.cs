8b42aa2fcf5d28e1ab3165d5c8ad07fa4dab0da9
Nick Babcock <nbabcock19@hotmail.com>
2018-05-03 16:16:07 -0500

    Add tests for culture invariant formatting

---
+++ OhmGraphite.Test/FormatMetricsTest.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Globalization;
+using System.Threading;
+using Xunit;
+
+namespace OhmGraphite.Test
+{
+    public class FormatMetricsTest
+    {
+        [Fact]
+        public void FormatGraphiteIdentifier()
+        {
+            var epoch = new DateTimeOffset(new DateTime(2001, 1, 13)).ToUnixTimeSeconds();
+            var sensor = new Sensor("my.cpu.identifier", "voltage", 1.06f);
+            string actual = MetricTimer.FormatGraphiteData("MY-PC", epoch, sensor);
+            Assert.Equal("ohm.MY-PC.my.cpu.identifier.voltage 1.06 979365600", actual);
+        }
+
+        [Fact]
+        public void FormatCultureInvariant()
+        {
+            CultureInfo original = Thread.CurrentThread.CurrentCulture;
+            try
+            {
+                // de-DE culture will format 1.06 as 1,06 which graphite doesn't like
+                Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
+
+                var epoch = new DateTimeOffset(new DateTime(2001, 1, 13)).ToUnixTimeSeconds();
+                var sensor = new Sensor("my.cpu.identifier", "voltage", 1.06f);
+                string actual = MetricTimer.FormatGraphiteData("MY-PC", epoch, sensor);
+                Assert.Equal("ohm.MY-PC.my.cpu.identifier.voltage 1.06 979365600", actual);
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentCulture = original;
+            }
+        }
+    }
+}
