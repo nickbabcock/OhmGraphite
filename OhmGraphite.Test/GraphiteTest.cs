433b9186dbef77f475506ac2878399582016151c
Nick Babcock <nbabcock19@hotmail.com>
2018-10-19 07:04:35 -0500

    Write graphite tag integration test

--- OhmGraphite.Test/GraphiteTest.cs
+++ OhmGraphite.Test/GraphiteTest.cs
@@ -21,5 +21,22 @@ namespace OhmGraphite.Test
             var content = await resp.Content.ReadAsStringAsync();
             Assert.Contains("ohm.my-pc.intelcpu.0.temperature.cpucore.1", content);
         }
+
+        [Fact, Trait("Category", "integration")]
+        public async void InsertTagGraphiteTest()
+        {
+            var writer = new GraphiteWriter("graphite", 2003, "my-pc", tags: true);
+            await writer.ReportMetrics(DateTime.Now, TestSensorCreator.Values());
+
+            // wait for carbon to sync to disk
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            var client = new HttpClient();
+            var resp = await client.GetAsync("http://graphite/render?format=csv&target=seriesByTag('sensor_type=Temperature','hardware_type=CPU')");
+            Assert.True(resp.IsSuccessStatusCode);
+            var content = await resp.Content.ReadAsStringAsync();
+            Assert.Contains("host=my-pc", content);
+            Assert.Contains("app=ohm", content);
+            Assert.Contains("sensor_type=Temperature", content);
+        }
     }
 }
