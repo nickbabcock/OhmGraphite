ab4d182a06d3d807e6aea73508e4be3945190721
Nick Babcock <nbabcock19@hotmail.com>
2018-10-20 11:53:51 -0500

    Simplify sensor collector for ci

--- OhmGraphite.Test/SensorCollectorTest.cs
+++ OhmGraphite.Test/SensorCollectorTest.cs
@@ -9,39 +9,33 @@ namespace OhmGraphite.Test
         [Fact]
         public void SensorsAddedWhenHardwareAdded()
         {
-            var computer = new Computer()
-            {
-                CPUEnabled = true,
-                FanControllerEnabled = false,
-                GPUEnabled = false,
-                HDDEnabled = false,
-                MainboardEnabled = false,
-                RAMEnabled = false
-            };
-
+            var computer = new Computer();
             var collector = new SensorCollector(computer);
+
             try
             {
                 collector.Open();
-                var firstCount = collector.ReadAllSensors().Count();
-                Assert.True(firstCount > 0, "firstCount > 0");
+                var unused = collector.ReadAllSensors().Count();
 
+                computer.CPUEnabled = true;
+                computer.FanControllerEnabled = true;
                 computer.MainboardEnabled = true;
                 computer.FanControllerEnabled = true;
                 computer.HDDEnabled = true;
                 computer.RAMEnabled = true;
 
-                var secondCount = collector.ReadAllSensors().Count();
-                Assert.True(secondCount > firstCount, "secondCount > firstCount");
+                var addedCount = collector.ReadAllSensors().Count();
+                Assert.True(addedCount > 0, "addedCount > 0");
 
+                computer.CPUEnabled = false;
+                computer.FanControllerEnabled = false;
                 computer.MainboardEnabled = false;
                 computer.FanControllerEnabled = false;
                 computer.HDDEnabled = false;
                 computer.RAMEnabled = false;
 
-                var thirdCount = collector.ReadAllSensors().Count();
-                Assert.True(thirdCount < secondCount, "thirdCount < secondCount");
-                Assert.True(thirdCount > 0, "thirdCount > 0");
+                var removedCount = collector.ReadAllSensors().Count();
+                Assert.True(addedCount > removedCount, "addedCount > removedCount");
             }
             finally
             {
