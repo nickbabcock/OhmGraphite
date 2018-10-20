4f315383a4705ee2aceb468815165abe6c374cf1
Nick Babcock <nbabcock19@hotmail.com>
2018-10-20 11:36:56 -0500

    Add sensor collector test

---
+++ OhmGraphite.Test/SensorCollectorTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using OpenHardwareMonitor.Hardware;
+using Xunit;
+
+namespace OhmGraphite.Test
+{
+    public class SensorCollectorTest
+    {
+        [Fact]
+        public void SensorsAddedWhenHardwareAdded()
+        {
+            var computer = new Computer()
+            {
+                CPUEnabled = true,
+                FanControllerEnabled = false,
+                GPUEnabled = false,
+                HDDEnabled = false,
+                MainboardEnabled = false,
+                RAMEnabled = false
+            };
+
+            var collector = new SensorCollector(computer);
+            try
+            {
+                collector.Open();
+                var firstCount = collector.ReadAllSensors().Count();
+                Assert.True(firstCount > 0);
+
+                computer.MainboardEnabled = true;
+                computer.FanControllerEnabled = true;
+                computer.HDDEnabled = true;
+                computer.RAMEnabled = true;
+
+                var secondCount = collector.ReadAllSensors().Count();
+                Assert.True(secondCount > firstCount);
+
+                computer.MainboardEnabled = false;
+                computer.FanControllerEnabled = false;
+                computer.HDDEnabled = false;
+                computer.RAMEnabled = false;
+
+                var thirdCount = collector.ReadAllSensors().Count();
+                Assert.True(thirdCount < secondCount);
+                Assert.True(thirdCount > 0);
+            }
+            finally
+            {
+                collector.Close();
+            }
+        }
+    }
+}
